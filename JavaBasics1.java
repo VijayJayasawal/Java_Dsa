7838811c0c537dc4256a8caaaf2e9b016d2991d2
Vijay Jayswal <jayswalvijay140@gmail.com>
2025-06-15 12:04:15 +0530

    modification

--- JavaBasics1.java
+++ JavaBasics1.java
@@ -2,6 +2,6 @@ package Java_Dsa;
 
 class javaBasics1 {
     public static void main(String[] args) {
-        System.out.println("Hello ddthrturrWorld!"); 
+        System.out.println("Helllo"); 
     }
 }
